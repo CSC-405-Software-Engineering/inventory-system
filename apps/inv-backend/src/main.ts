9be8a4066ce2b02980ba98b1bf42acc6d91721cc
georgeuwagbale <uwagbalegeorge@gmail.com>
2024-02-20 18:43:32 +0100

    Project Setup

--- apps/inv-backend/src/main.ts
+++ apps/inv-backend/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enables API versioning
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'v',
+  });
+
   app.setGlobalPrefix("/backend")
   await app.listen(3000);
 }
--- apps/inv-backend/src/typeorm.config.ts
+++ apps/inv-backend/src/typeorm.config.ts
@@ -6,9 +6,10 @@ require('dotenv').config();
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT),
   username: process.env.POSTGRES_USERNAME,
   database: process.env.POSTGRES_DATABASE,
-  password: process.env.POSTGRES_PASSWORD,
+  password: "postgres",
   url: process.env.POSTGRES_URL,
   entities: [],
   migrations: [__dirname + '/../database/migrations/*{.ts,.js}'], // Migration files directory
