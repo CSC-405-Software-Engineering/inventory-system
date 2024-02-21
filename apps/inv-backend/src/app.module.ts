720e4358bfce8bfe184db095d397b20d997f5f09
georgeuwagbale <uwagbalegeorge@gmail.com>
2024-02-21 22:09:17 +0100

    Add TypeOrmModule import and forFeature method call in stock.module.ts

--- apps/inv-backend/src/app.module.ts
+++ apps/inv-backend/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './typeorm.config';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { InventoryModule } from './modules/inventory/inventory.module';
 import { StockModule } from './modules/stock/stock.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
--- apps/inv-backend/src/modules/stock/stock.module.ts
+++ apps/inv-backend/src/modules/stock/stock.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { StockController } from './stock.controller';
 import { StockService } from './stock.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Stock } from './entities/stock.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Stock])],
   controllers: [StockController],
   providers: [StockService]
 })
