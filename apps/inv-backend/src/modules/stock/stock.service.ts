473f1e5c0fe9e6bc8ffb0316004a6957835dd18c
nwaniayo <nwaniayomilala@gmail.com>
2024-02-21 21:42:20 +0100

    Implement create and update for stock

--- apps/inv-backend/src/modules/stock/stock.service.ts
+++ apps/inv-backend/src/modules/stock/stock.service.ts
@@ -1,11 +1,33 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Stock } from './entities/stock.entity'; // Import your Stock entity
+import { CreateStockDto } from './dto/createStock.dto'; // Import your DTO
+import { UpdateStockDto } from './dto/updateStock.dto';
 @Injectable()
 export class StockService {
+  constructor(
+    @InjectRepository(Stock)
+    private readonly inventoryRepository: Repository<Stock>,
+  ) {}
 
-    async create() {
+  async create(createStockDto: CreateStockDto): Promise<Stock> {
+    
+      const existingInventory = await this.inventoryRepository.findOne({
+        where: { id: createStockDto.inventoryId },
+      });
 
+      if (existingInventory) {
+        throw new Error('Inventory already exists for the provided product ID');
+      }
+
+      const newInventory = this.inventoryRepository.create(createStockDto);
+
+      return await this.inventoryRepository.save(newInventory);
     }
+  
+
+
 
     async findAll() {
 
@@ -15,10 +37,21 @@ export class StockService {
 
     }
 
-    async update() {
-
-    }
-
+    async update(id: number, updateStockDto: UpdateStockDto): Promise<Stock> {
+        const existingInventory = await this.inventoryRepository.findOne(id);
+    
+        if (!existingInventory) {
+          throw new NotFoundException('Stock not found');
+        }
+    
+        // Update properties of existing inventory with the ones provided in updateStockDto
+        this.inventoryRepository.merge(existingInventory, updateStockDto);
+    
+        return await this.inventoryRepository.save(existingInventory);
+      }
+    
+    
+    
     async remove() {
     }
 }
