c3465531ce07fe40d8d4a7619ed7cae618358471
georgeuwagbale <uwagbalegeorge@gmail.com>
2024-02-21 18:36:10 +0100

    Created the auth and users module

--- apps/inv-backend/src/app.module.ts
+++ apps/inv-backend/src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './users/users.module';
 import { typeOrmConfig } from './typeorm.config';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
 
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
+    AuthModule,
     UsersModule,],
   controllers: [AppController],
   providers: [AppService],
