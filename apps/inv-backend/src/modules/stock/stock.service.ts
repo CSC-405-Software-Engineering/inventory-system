2c2342ee06c80dcf14b555c5c522c5a8a471ceaa
georgeuwagbale <uwagbalegeorge@gmail.com>
2024-02-21 21:45:36 +0100

    Implemented CRUD for StockService

--- apps/inv-backend/src/modules/stock/stock.service.ts
+++ apps/inv-backend/src/modules/stock/stock.service.ts
@@ -1,24 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateStockDto } from './dto/createStock.dto';
+import { In, Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Stock } from './entities/stock.entity';
 
 @Injectable()
 export class StockService {
 
-    async create() {
+    constructor(
+        @InjectRepository(Stock) private readonly stockRepository: Repository<Stock>,
+    ) {}
 
+    async create(createStockDto: CreateStockDto) {
+        const stock = await this.stockRepository.findOne({ where: { name: createStockDto.name } });
+
+        if (stock) {
+            throw new NotFoundException(`Stock with name ${createStockDto.name} already exists`);
+        }
+
+        const newStock = this.stockRepository.create(createStockDto);
+        return await this.stockRepository.save(newStock);
     }
 
     async findAll() {
+        return await this.stockRepository.find();
+    }
+
+    async findOne(id: string) {
+        const stock = await this.stockRepository.findOne({ where: { id } });
+
+        if (!stock) {
+            throw new NotFoundException(`Stock with ID ${id} not found`);
+        }
+
+        return stock;
+    }
+
+    async update(id: string, stock: Stock) {
+        const existingStock = await this.stockRepository.findOne({ where: { id } });
+
+        if (!existingStock) {
+            throw new NotFoundException(`Stock with ID ${id} not found`);
+        }
+
+        this.stockRepository.merge(existingStock, stock);
+        return await this.stockRepository.save(existingStock);
 
     }
 
-    async findOne() {
+    async remove(id: string) {
+        const stock = await this.stockRepository.findOne({ where: { id } });
 
-    }
+        if (!stock) {
+            throw new NotFoundException(`Stock with ID ${id} not found`);
+        }
 
-    async update() {
-
-    }
-
-    async remove() {
+        return await this.stockRepository.remove(stock);
     }
 }
