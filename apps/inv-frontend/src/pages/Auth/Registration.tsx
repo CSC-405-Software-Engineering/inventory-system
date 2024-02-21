c48477a00592bbaf1772f906a3fb1705dacbc649
francisojeah <ojeahfrancis@gmail.com>
2024-02-21 18:25:33 +0100

    setup registration page

---
+++ apps/inv-frontend/src/pages/Auth/Registration.tsx
@@ -0,0 +1,11 @@
+import DashboardLayout from "@/components/DashboardLayout";
+
+const Registration = () => {
+  return (
+    <DashboardLayout>
+      <div>Registration</div>
+    </DashboardLayout>
+  );
+};
+
+export default Registration;
