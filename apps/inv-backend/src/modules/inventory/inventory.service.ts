c1979f897681c7ec5b7282a1a25a15b5645e12d1
OGB <chidubem.info@gmail.com>
2024-02-21 20:29:51 +0100

    create and update inventory implemented

--- apps/inv-backend/src/modules/inventory/inventory.service.ts
+++ apps/inv-backend/src/modules/inventory/inventory.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Inventory } from './entities/inventory.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class InventoryService {}
+export class InventoryService {
+    constructor(
+        @InjectRepository(Inventory)
+        private readonly inventoryRepository: Repository<Inventory>,
+    ) {}
+
+    async createInventory(inventory: Inventory): Promise<Inventory> {
+        const newInventory = this.inventoryRepository.create(inventory);
+        return await this.inventoryRepository.save(newInventory);
+    }
+
+    async updateInventory(id: string, inventory:Inventory): Promise<Inventory> {
+        const existingInventory = await this.inventoryRepository.findOne({ where: { id } });
+
+        if (!existingInventory) {
+            throw new NotFoundException(`Inventory with ID ${id} not found`);
+        }
+
+        this.inventoryRepository.merge(existingInventory, inventory);
+        return await this.inventoryRepository.save(existingInventory);
+    }
+}
