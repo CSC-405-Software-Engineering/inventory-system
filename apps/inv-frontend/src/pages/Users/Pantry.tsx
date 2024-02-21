aec82e479d00dad6b025f3e1275e4a6e3744fc38
gracetswanya <gracetswanya@gmail.com>
2024-02-21 23:45:29 +0100

    Frontend code for Quantity Settings

--- apps/inv-frontend/src/pages/Users/Pantry.tsx
+++ apps/inv-frontend/src/pages/Users/Pantry.tsx
@@ -1,9 +1,25 @@
 import DashboardLayout from "@/components/DashboardLayout";
+import EnrollCourseModal from "@/components/EditItemQuantityModal";
+import { useState } from "react";
 
 const Pantry = () => {
+
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleEditItemQuantity = () => {
+    setOpenModal(true);
+  };
+
   return (
     <DashboardLayout>
+      <>
       <div>Pantry</div>
+      <button onClick={handleEditItemQuantity}>click me</button>
+      <EnrollCourseModal
+        openModal={openModal}
+        setOpenModal={setOpenModal}
+      />
+      </>
     </DashboardLayout>
   );
 };
