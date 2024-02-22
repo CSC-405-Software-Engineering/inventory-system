1e4258c9ad8e760fb74c225254b264a0054d823c
georgeuwagbale <uwagbalegeorge@gmail.com>
2024-02-22 04:43:59 +0100

    Add versioning to inventory controller

--- apps/inv-backend/src/modules/inventory/inventory.controller.ts
+++ apps/inv-backend/src/modules/inventory/inventory.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Version } from '@nestjs/common';
 import { InventoryService } from './inventory.service';
 import { CreateInventoryDto } from './dto/createInventory.dto';
 
@@ -6,26 +6,31 @@ import { CreateInventoryDto } from './dto/createInventory.dto';
 export class InventoryController {
     constructor(private readonly inventoryService: InventoryService) {}
     
+    @Version('1')
     @Post()
     async create(@Body() createInventoryDto: CreateInventoryDto) {
       return await this.inventoryService.create(createInventoryDto);
     }
     
+    @Version('1')
     @Get()
     async findAll() {
       return await this.inventoryService.findAll();
     }
     
+    @Version('1')
     @Get(':id')
     async findOne(@Param('id') id: string) {
       return await this.inventoryService.findOne(id);
     }
 
+    @Version('1')
     @Patch(':id')
     async update(@Param('id') id: string, @Body() updateInventoryDto: CreateInventoryDto) {
       return await this.inventoryService.update(id, updateInventoryDto);
     }
   
+    @Version('1')
     @Delete(':id')
     async remove(@Param('id') id: string) {
       return await this.inventoryService.remove(id);
