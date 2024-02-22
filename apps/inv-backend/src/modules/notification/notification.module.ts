58906967a69953108bef7ffeeec686d6723394c3
kaobimdi iwelumo <kaobimdi.iwelumo@pau.edu.ng>
2024-02-22 02:00:27 +0100

    Notification for Maximum Stock reached

--- apps/inv-backend/src/modules/notification/notification.module.ts
+++ apps/inv-backend/src/modules/notification/notification.module.ts
@@ -15,8 +15,8 @@ require('dotenv').config();
         port: 465,
         secure: true,
         auth: {
-          user: 'zionchrist8@gmail.com',
-          pass: 'hnvs dqdg mzga fwbm',
+          user: process.env.MAIL_EMAIL,
+          pass: process.env.MAIL_PASSWORD,
         },
         connectionTimeout: 10000,
         logger: true,
