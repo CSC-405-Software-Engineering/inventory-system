ed243e97fcab08ab0a792de8affbe0f32bd31bd6
georgeuwagbale <uwagbalegeorge@gmail.com>
2024-02-22 04:43:17 +0100

    Update inventory.controller.ts with new routes

--- apps/inv-backend/src/modules/inventory/inventory.controller.ts
+++ apps/inv-backend/src/modules/inventory/inventory.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { InventoryService } from './inventory.service';
 import { CreateInventoryDto } from './dto/createInventory.dto';
 
@@ -16,6 +16,15 @@ export class InventoryController {
       return await this.inventoryService.findAll();
     }
     
+    @Get(':id')
+    async findOne(@Param('id') id: string) {
+      return await this.inventoryService.findOne(id);
+    }
+
+    @Patch(':id')
+    async update(@Param('id') id: string, @Body() updateInventoryDto: CreateInventoryDto) {
+      return await this.inventoryService.update(id, updateInventoryDto);
+    }
   
     @Delete(':id')
     async remove(@Param('id') id: string) {
