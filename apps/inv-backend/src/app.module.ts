01b89294c7bfb5ec237da61755d98c46ba79daaf
Zio-n <zionchrist8@gmail.com>
2024-02-22 01:52:52 +0100

    Fixed Issues

--- apps/inv-backend/src/app.module.ts
+++ apps/inv-backend/src/app.module.ts
@@ -4,27 +4,20 @@ import { AppService } from './app.service';
 import { typeOrmConfig } from './typeorm.config';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
-<<<<<<< HEAD
 import { NotificationModule } from './modules/notification/notification.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
-=======
 import { InventoryModule } from './modules/inventory/inventory.module';
 import { StockModule } from './modules/stock/stock.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
->>>>>>> origin/development
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
     AuthModule,
     UsersModule,
-<<<<<<< HEAD
-    NotificationModule,EventEmitterModule.forRoot()],
-=======
     InventoryModule,
-    StockModule,],
->>>>>>> origin/development
+    StockModule,
+    NotificationModule,EventEmitterModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
