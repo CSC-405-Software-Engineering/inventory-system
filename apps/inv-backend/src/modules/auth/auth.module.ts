c6f20cee77a67d0d6915823208b4b9055eb47140
georgeuwagbale <uwagbalegeorge@gmail.com>
2024-02-22 04:39:17 +0100

    Remove unused import in auth.module.ts

--- apps/inv-backend/src/modules/auth/auth.module.ts
+++ apps/inv-backend/src/modules/auth/auth.module.ts
@@ -3,7 +3,6 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
-import { jwtConstants } from './auth.constant';
 import { Auth } from './entities/auth.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
