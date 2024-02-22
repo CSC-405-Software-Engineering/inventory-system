0bc6ce882d3fd218311b33663ff854fed750510f
georgeuwagbale <uwagbalegeorge@gmail.com>
2024-02-22 10:23:17 +0100

    Add @nestjs/config and passport-jwt dependencies

---
+++ apps/inv-backend/src/modules/auth/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: any) {
+    return { id: payload.sub };
+  }
+}
