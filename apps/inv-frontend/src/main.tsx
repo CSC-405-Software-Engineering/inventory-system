1f64cfe0398f641672bb51201444352b0fc0085f
francisojeah <ojeahfrancis@gmail.com>
2024-02-24 23:02:46 +0100

    updated state management

--- apps/inv-frontend/src/main.tsx
+++ apps/inv-frontend/src/main.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-// import { loadUser } from './store/slices/authSlice.ts';
-// import { store } from './store/store.ts';
+import { loadUser } from './store/slices/authSlice.ts';
+import { store } from './store/store.ts';
 
 
-// store.dispatch<any>(loadUser());
+store.dispatch<any>(loadUser());
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
