d687222e3cdf26a1bd2b814ff91331b9024e857e
francisojeah <ojeahfrancis@gmail.com>
2024-02-25 14:09:08 +0100

    build updates

--- apps/inv-frontend/src/components/MetaTags.tsx
+++ apps/inv-frontend/src/components/MetaTags.tsx
@@ -14,17 +14,17 @@ const MetaTags = ({ title, description, keywords, pageUrl }: MetaTagsProps) => {
       <meta name="keywords" content={keywords} />
       <meta name="description" content={description} />
       <meta charSet="utf-8" />
-      <link rel="icon" href={'/images/tlogo.svg'} />
+      <link rel="icon" href={'/assets/icons/app-icon.svg'} />
       <link rel="canonical" href={`${pageUrl}`} />
       <title>{title}</title>
       <meta name="og:title" content={title} />
       <meta name="og:description" content={description} />
-      <meta name="og:image" content={'/images/tlogo.svg'} />
+      <meta name="og:image" content={'/assets/icons/app-icon.svg'} />
       <meta name="og:url" content={`${pageUrl}`} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={'/images/tlogo.svg'} />
+      <meta name="twitter:image" content={'/assets/icons/app-icon.svg'} />
     </Helmet>
   );
 };
